2d6b27612d2ed9409aad6333c6e96c844b2a11ce
EyobAshenaki <ethioeyoba@gmail.com>
2022-05-08 16:44:57 +0300

    feat(layout): add student layout

---
+++ layouts/student.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <app-header />
+    <student-side-bar />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import StudentSideBar from '~/components/StudentSideBar.vue'
+export default {
+  name: 'NavLayout',
+  components: { AppHeader, StudentSideBar },
+  // comments: {
+  //   AppHeader,
+  //   StudentSideBar
+  // },
+  data() {
+    return {}
+  },
+}
+</script>
