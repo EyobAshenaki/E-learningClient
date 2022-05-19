1e20e276d64c98d23fb28055cd047fbf508357d1
BettyBane <www.beteldessalegn99@gmail.com>
2022-05-19 23:29:32 +0300

    feat: create department page with tabbed student, teacher tables,link classes details button to it

---
+++ pages/administrator/_id/department/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="11" class="mt-10">
+        <div>
+          <v-breadcrumbs :items="items"></v-breadcrumbs>
+        </div>
+        <AdminDeptTableTabs />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'administrator',
+  name: 'DepartmentIndexPage',
+  data: () => ({
+    items: [
+      {
+        text: 'Dashboard',
+        disabled: false,
+        href: '/administrator/_id',
+      },
+      {
+        text: 'Department',
+        disabled: true,
+        href: '/administrator/_id/department',
+      },
+    ],
+  }),
+}
+</script>
