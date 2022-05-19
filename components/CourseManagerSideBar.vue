bbbc6e946d59f805687fefd8fe3d5f1d02cd5281
EyobAshenaki <ethioeyoba@gmail.com>
2022-05-19 14:30:51 +0300

    chore(courseManager): add sidebar and layout for course manager page

---
+++ components/CourseManagerSideBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="true"
+    mini-variant-width="100"
+    clipped
+    app
+    permanent
+  >
+    <v-list class="pa-0">
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        link
+        class="px-0"
+        @click="changePage(item.title)"
+      >
+        <v-tooltip top>
+          <template #activator="{ on, attrs }">
+            <v-list-item-icon class="py-2" v-bind="attrs" v-on="on">
+              <v-icon size="30">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+          </template>
+          <span>{{ item.title }}</span>
+        </v-tooltip>
+
+        <!-- <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content> -->
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'My Courses', icon: 'mdi-bookshelf' },
+      ],
+      mini: true,
+    }
+  },
+  watch: {},
+  methods: {
+    changePage(name) {
+      if (name === 'Dashboard') {
+        this.$router.push({ name: 'courseManager-id', params: { id: 1 } })
+      } else if (name === 'My Courses') {
+        this.$router.push({
+          name: 'courseManager-id-courses',
+          params: { id: 1 },
+        })
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/courseManager.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <app-header />
+    <course-manager-side-bar />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import CourseManagerSideBar from '~/components/CourseManagerSideBar.vue'
+export default {
+  name: 'NavLayout',
+  components: { AppHeader, CourseManagerSideBar },
+  data() {
+    return {}
+  },
+}
+</script>
