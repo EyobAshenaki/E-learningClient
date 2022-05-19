6308392b6861a6fce8d481966b1467a494acd837
BettyBane <www.beteldessalegn99@gmail.com>
2022-05-19 18:31:29 +0300

    feat: fix id mapping, integrate backend sans delete, in classes.remove students in dep

--- components/AdminClassesClasses.vue
+++ components/AdminClassesClasses.vue
@@ -35,13 +35,13 @@
               <v-card-text>
                 <v-container>
                   <v-row>
-                    <v-col cols="12" sm="6" md="4">
+                    <!-- <v-col cols="12" sm="6" md="4">
                       <v-text-field
                         v-model="editedItem.id"
                         label="ID"
                         type="number"
                       ></v-text-field>
-                    </v-col>
+                    </v-col> -->
                     <v-col cols="12" sm="6" md="4">
                       <v-text-field
                         v-model="editedItem.section"
@@ -135,26 +135,26 @@ export default {
         sortable: false,
         value: 'id',
       },
-      { text: 'Section', value: 'section' },
-      { text: 'Year', value: 'year' },
-      { text: 'Department', value: 'department' },
+      { text: 'Section', value: 'section', sortable: false },
+      { text: 'Year', value: 'year', sortable: false },
+      { text: 'Department', value: 'department', sortable: false },
       // // { text: 'Protein (g)', value: 'protein' },
       { text: 'Actions', value: 'actions', sortable: false },
     ],
     classes: [],
     editedIndex: -1,
     editedItem: {
-      id: 0,
+      dbId: null,
       section: '',
-      year: 0,
-      department: '',
+      year: null,
+      department: null,
       // protein: 0,
     },
     defaultItem: {
-      id: 0,
+      dbId: null,
       section: '',
-      year: 0,
-      department: '',
+      year: null,
+      department: null,
       // protein: 0,
     },
   }),
@@ -181,44 +181,69 @@ export default {
 
   methods: {
     // ...mapActions(['viewStudents']),
-    initialize() {
-      this.classes = [
-        {
-          id: 1,
-          section: 'A',
-          year: 5,
-          department: '24',
-          // protein: 4.0,
-        },
-        {
-          id: 2,
-          section: 'B',
-          year: 4,
-          department: 37,
-          // protein: 4.3,
-        },
-        {
-          id: 3,
-          section: 'C',
-          year: 1,
-          department: '23',
-          // protein: 6.0,
-        },
-        {
-          id: 4,
-          section: 'D',
-          year: 3,
-          department: '67',
-          // protein: 4.3,
-        },
-        {
-          id: 5,
-          section: 'E',
-          year: 2,
-          department: '49',
-          // protein: 3.9,
-        },
-      ]
+    async initialize() {
+      const query = `query class {
+                        studentClasses {
+                          dbId: id
+                          year
+                          section
+                          department {
+                            name
+                          }
+                        }
+                      }`
+      const studClassResponse = await this.$axios.post(
+        'http://localhost:4000/graphql',
+        { query }
+      )
+      if (studClassResponse.data.errors?.length) {
+        console.log(studClassResponse.data.errors[0].message)
+        throw new Error(studClassResponse.data.errors[0].message)
+      }
+      this.classes = studClassResponse.data.data.studentClasses
+      // this.classes = [
+      //   {
+      //     id: 1,
+      //     section: 'A',
+      //     year: 5,
+      //     department: '24',
+      //     // protein: 4.0,
+      //   },
+      //   {
+      //     id: 2,
+      //     section: 'B',
+      //     year: 4,
+      //     department: 37,
+      //     // protein: 4.3,
+      //   },
+      //   {
+      //     id: 3,
+      //     section: 'C',
+      //     year: 1,
+      //     department: '23',
+      //     // protein: 6.0,
+      //   },
+      //   {
+      //     id: 4,
+      //     section: 'D',
+      //     year: 3,
+      //     department: '67',
+      //     // protein: 4.3,
+      //   },
+      //   {
+      //     id: 5,
+      //     section: 'E',
+      //     year: 2,
+      //     department: '49',
+      //     // protein: 3.9,
+      //   },
+      // ]
+      this.classes = this.classes.map((clas, idx) => {
+        return {
+          ...clas,
+          id: idx + 1,
+        }
+      })
     },
 
     editItem(item) {
@@ -254,10 +279,53 @@ export default {
       })
     },
 
-    save() {
+    async save() {
       if (this.editedIndex > -1) {
+        const query = `mutation uc($id: ID!, $year: String!, $section: String!) {
+                          updateStudentClass(
+                            updateStudentClassInput: { id: $id, year: $year, section: $section }
+                          ) {
+                            dbId: id
+                            year
+                            section
+                          }
+                        }`
+        const variables = {
+          id: this.editedItem.dbId,
+          year: this.editedItem.year,
+          section: this.editedItem.section,
+        }
+        const editedClass = await this.$axios.post(
+          'http://localhost:4000/graphql',
+          { query, variables }
+        )
+        if (editedClass.data.errors?.length) {
+          console.log(editedClass.data.errors[0].message)
+          throw new Error(editedClass.data.errors[0].message)
+        }
         Object.assign(this.classes[this.editedIndex], this.editedItem)
       } else {
+        const query = `mutation cc($year: String!, $section: String!) {
+                          createStudentClass(
+                            createStudentClassInput: { year: $year, section: $section }
+                          ) {
+                            dbId: id
+                            year
+                            section
+                          }
+                        }`
+        const variables = {
+          year: this.editedItem.year,
+          section: this.editedItem.section,
+        }
+        const newClass = await this.$axios.post(
+          'http://localhost:4000/graphql',
+          { query, variables }
+        )
+        if (newClass.data.errors?.length) {
+          console.log(newClass.data.errors[0].message)
+          throw new Error(newClass.data.errors[0].message)
+        }
         this.classes.push(this.editedItem)
       }
       this.close()
