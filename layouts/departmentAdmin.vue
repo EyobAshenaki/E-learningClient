450c95f0507cb9718297f2fdfdb7057c82374352
EyobAshenaki <ethioeyoba@gmail.com>
2022-05-28 20:38:01 +0300

    chore(layout): arrange layout for departmentAdministrator pages
    - added resources necessary for departmentAdmin pages

---
+++ layouts/departmentAdmin.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <app-header />
+    <department-admin-side-bar />
+    <v-main class="grey lighten-4">
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  import AppHeader from '@/components/AppHeader.vue'
+  import DepartmentAdminSideBar from '@/components/DepartmentAdminSideBar.vue'
+  export default {
+    name: 'DepartmentAdminLayout',
+    components: { AppHeader, DepartmentAdminSideBar },
+
+    data() {
+      return {}
+    },
+  }
+</script>
