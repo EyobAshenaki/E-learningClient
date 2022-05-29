380b100b849c9f727cd9057bcff70fe36342d599
Ezira Ashenafi <eazash22@gmail.com>
2022-05-29 16:57:44 +0300

    feat: refactor toast component

--- components/toast-snackbar.vue
+++ components/toast-snackbar.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-snackbar v-model="display" :color="statusColor">
+  <v-snackbar
+    v-model="toast"
+    :color="statusColor"
+    timeout="3000"
+    transition="fade-transition"
+  >
     {{ message || 'Unknown Error' }}
     <template #action="{ attrs }">
       <v-btn v-bind="attrs" icon @click="display = false">
@@ -10,50 +15,44 @@
 </template>
 
 <script>
-export default {
-  props: {
-    activator: {
-      type: Boolean,
-      required: true,
-    },
-    content: {
-      type: Object,
-      required: true,
-    },
-  },
-  computed: {
-    display: {
-      get() {
-        return this.activator
-      },
-      set(value) {
-        this.$emit('update:activator', value)
-      },
-    },
-    message() {
-      return this.content.message
-    },
-    statusColor() {
-      let color
-      switch (this.content.status) {
-        case 'SUCCESS': {
-          color = 'green'
-          break
-        }
-        case 'WARNING': {
-          color = 'orange lighten-1'
-          break
-        }
-        case 'ERROR': {
-          color = 'red lighten-2'
-          break
-        }
-        default: {
-          color = 'red'
-        }
+  export default {
+    data() {
+      return {
+        toast: false,
+        toastContent: {},
       }
-      return color
     },
-  },
-}
+    computed: {
+      message() {
+        return this.toastContent.message
+      },
+      statusColor() {
+        let color
+        switch (this.toastContent.status) {
+          case 'SUCCESS': {
+            color = 'green'
+            break
+          }
+          case 'WARNING': {
+            color = 'orange lighten-1'
+            break
+          }
+          case 'ERROR': {
+            color = 'red lighten-2'
+            break
+          }
+          default: {
+            color = 'red'
+          }
+        }
+        return color
+      },
+    },
+    mounted() {
+      this.$nuxt.$on('toast-notification', (event) => {
+        this.toast = true
+        this.toastContent = event
+      })
+    },
+  }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,25 +4,6 @@
     <v-container class="main-container" fluid>
       <Nuxt />
     </v-container>
-    <ToastSnackbar :activator.sync="toast" :content="toastContent" />
+    <ToastSnackbar />
   </v-app>
 </template>
-
-<script>
-import ToastSnackbar from '~/components/toast-snackbar.vue'
-export default {
-  components: { ToastSnackbar },
-  data() {
-    return {
-      toast: false,
-      toastContent: {},
-    }
-  },
-  created() {
-    this.$nuxt.$on('toast-notification', (event) => {
-      this.toast = true
-      this.toastContent = event
-    })
-  },
-}
-</script>
