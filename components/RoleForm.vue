6478f2b7a6a3d660b6dbc0135472ea3e2b64c403
Ezira Ashenafi <eazash22@gmail.com>
2022-06-03 19:06:22 +0300

    feat(admin): add students page

---
+++ components/RoleForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-col cols="12">
+    <v-select
+      v-model="accountRoles"
+      required
+      label="Roles"
+      multiple
+      :items="roleItems"
+    />
+  </v-col>
+</template>
+
+<script>
+  export default {
+    props: {
+      roles: {
+        type: Array,
+        default: () => [],
+      },
+    },
+    data() {
+      return {
+        roleItems: ['DEV', 'ADMINISTRATOR', 'STUDENT'],
+        accountRoles: this.roles.map((role) => role.name),
+      }
+    },
+  }
+</script>
