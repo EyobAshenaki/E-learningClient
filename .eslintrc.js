5fb3ff60de1baefccfd7fa2b479b252568f68219
Ezira Ashenafi <eazash22@gmail.com>
2022-06-03 19:06:56 +0300

    fix(eslint): allow for v-slot modifiers

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,11 @@ module.exports = {
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': 'off',
+    'vue/valid-v-slot': [
+      'error',
+      {
+        allowModifiers: true,
+      },
+    ],
   },
 }
