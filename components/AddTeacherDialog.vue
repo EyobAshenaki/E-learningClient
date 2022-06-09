211bc1d269076c12bccea030e931fc0b3d06f709
Ezra Ashenafi <eazash22@gmail.com>
2022-06-09 11:20:32 +0300

    feat: added teacher dialogue

---
+++ components/AddTeacherDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <template #activator="{ on, attrs }">
+      <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+        Add Teacher Account(s)
+      </v-btn>
+    </template>
+    <v-card v-if="dialog" flat>
+      <v-card-title class="primary--text"
+        ><span class="text-h5">Add Teacher Account(s)</span></v-card-title
+      >
+      <v-container fluid>
+        <v-radio-group v-model="selectedComponent" row mandatory>
+          <v-radio label="Single Input" value="addTeacherForm" />
+          <v-radio label="Multiple Input" value="importTeachersForm" />
+        </v-radio-group>
+      </v-container>
+      <component
+        :is="selectedComponent"
+        @close="dialog = false"
+        @save="success"
+      ></component>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  import addStudentForm from '@/components/AddStudentFrom.vue'
+  import importStudentsForm from '@/components/ImportStudentsForm.vue'
+  export default {
+    components: {
+      addStudentForm,
+      importStudentsForm,
+    },
+    data() {
+      return {
+        selectedComponent: '',
+        dialog: false,
+      }
+    },
+    methods: {
+      success() {
+        this.dialog = false
+        this.$emit('complete')
+      },
+    },
+  }
+</script>
