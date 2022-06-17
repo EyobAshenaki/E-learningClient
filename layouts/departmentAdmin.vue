3042e8084f79f132ba29ab29650e74218b2a70e9
EyobAshenaki <ethioeyoba@gmail.com>
2022-06-17 12:07:59 +0300

    fix: patch after rebase issues

--- layouts/departmentAdmin.vue
+++ layouts/departmentAdmin.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-    <app-header />
     <department-admin-side-bar />
     <v-main class="grey lighten-4">
       <v-container>
@@ -11,11 +10,12 @@
 </template>
 
 <script>
-  import AppHeader from '@/components/AppHeader.vue'
   import DepartmentAdminSideBar from '@/components/DepartmentAdminSideBar.vue'
   export default {
     name: 'DepartmentAdminLayout',
-    components: { AppHeader, DepartmentAdminSideBar },
+    components: {
+      DepartmentAdminSideBar,
+    },
 
     data() {
       return {}
