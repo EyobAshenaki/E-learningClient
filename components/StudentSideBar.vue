4e23d4dfd05ec99e2e3df9c1a9d62e8a8ae25e56
EyobAshenaki <ethioeyoba@gmail.com>
2022-06-18 15:42:32 +0300

    fix(studentPage): patch student sidebar for better routing

--- components/StudentSideBar.vue
+++ components/StudentSideBar.vue
@@ -1,69 +1,71 @@
 <template>
-  <v-navigation-drawer
-    v-model="drawer"
-    :mini-variant="true"
-    mini-variant-width="100"
-    clipped
-    app
-    permanent
-  >
-    <v-list class="pa-0">
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        link
-        class="px-0"
-        @click="changePage(item.title)"
-      >
-        <v-tooltip top>
-          <template #activator="{ on, attrs }">
-            <v-list-item-icon class="py-2" v-bind="attrs" v-on="on">
+  <v-navigation-drawer v-model="drawer" clipped app permanent>
+    <v-list>
+      <v-tooltip v-for="item in items" :key="item.title" right>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" exact nuxt :to="item.route" v-on="on">
+            <v-list-item-icon>
               <v-icon size="30">{{ item.icon }}</v-icon>
             </v-list-item-icon>
-          </template>
-          <span>{{ item.title }}</span>
-        </v-tooltip>
-
-        <!-- <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content> -->
-      </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+        <span>{{ item.description || item.title }}</span>
+      </v-tooltip>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      drawer: true,
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'My Courses', icon: 'mdi-bookshelf' },
-        { title: 'Assignments', icon: 'mdi-note-edit' },
-        { title: 'Quizzes', icon: 'mdi-text-box-check' },
-        { title: 'Calendar', icon: 'mdi-calendar-month' },
-      ],
-      mini: true,
-    }
-  },
-  watch: {},
-  methods: {
-    changePage(name) {
-      if (name === 'Dashboard') {
-        this.$router.push({ name: 'student-id', params: { id: 1 } })
-      } else if (name === 'My Courses') {
-        this.$router.push({ name: 'student-id-courses', params: { id: 1 } })
-      } else if (name === 'Assignments') {
-        this.$router.push({ name: 'student-id-assignments', params: { id: 1 } })
-      } else if (name === 'Quizzes') {
-        this.$router.push({ name: 'student-id-quizzes', params: { id: 1 } })
-      } else if (name === 'Calendar') {
-        this.$router.push({ name: 'student-id-calendar', params: { id: 1 } })
+  export default {
+    data() {
+      return {
+        drawer: true,
+        items: [
+          {
+            title: 'Dashboard',
+            icon: 'mdi-view-dashboard',
+            route: {
+              name: 'student-id',
+              params: { id: this.$nuxt.context.params.id },
+            },
+          },
+          {
+            title: 'My Courses',
+            icon: 'mdi-bookshelf',
+            route: {
+              name: 'student-id-courses',
+              params: { id: this.$nuxt.context.params.id },
+            },
+          },
+          {
+            title: 'Assignments',
+            icon: 'mdi-note-edit',
+            route: {
+              name: 'student-id-assignments',
+              params: { id: this.$nuxt.context.params.id },
+            },
+          },
+          {
+            title: 'Quizzes',
+            icon: 'mdi-text-box-check',
+            route: {
+              name: 'student-id-quizzes',
+              params: { id: this.$nuxt.context.params.id },
+            },
+          },
+          // {
+          //   title: 'Calendar',
+          //   icon: 'mdi-calendar-month',
+          //   route: {
+          //     name: 'student-id-calendar',
+          //     params: { id: this.$nuxt.context.params.id },
+          //   },
+          // },
+        ],
       }
     },
-  },
-}
+  }
 </script>
-
-<style lang="scss" scoped></style>
