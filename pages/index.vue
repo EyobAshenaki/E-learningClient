112f6b1bb02230c8103ce9c10ce3d8a7bf040b0c
BettyBane <www.beteldessalegn99@gmail.com>
2022-06-19 22:13:56 +0300

    feat: add landing page, remove nav bar from guest layout, add png in assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,63 @@
 <template>
-  <div>
-    <v-row justify="center" align="center"> </v-row>
-    <!-- <CourseWhatYoullLearn />
-  <CourseCourseContent />
-  <CourseRequirements /> -->
-    <TrialComponent />
-    <!-- <AdminAllAccounts /> -->
-  </div>
+  <v-row
+    justify="center"
+    align="center"
+    class="d-flex"
+    style="
+      background-image: linear-gradient(135deg, #0156d2, #d201bf);
+      opacity: 0.8;
+    "
+  >
+    <v-col cols="12" class="mx-auto">
+      <v-row class="mt-4">
+        <v-col cols="6">
+          <p
+            class="text-h2 orange--text text--lighten-4"
+            style="
+              margin-top: 100px;
+              font-family: 'Lucida Handwriting', cursive;
+            "
+          >
+            Welcome To The AASTU <br />
+            E-learning Portal
+          </p>
+          <p
+            class="text-h4 orange--text text--lighten-4"
+            style="
+              margin-top: 30px;
+              margin-bottom: 100px;
+              font-family: 'Lucida Handwriting', cursive;
+            "
+          >
+            Take the steps necessary to digitalize your learning experience
+          </p>
+          <v-btn
+            elevation="24"
+            x-large
+            color="orange lighten-4"
+            style="margin-left: 350px"
+            :to="{ name: 'login' }"
+            >LOG IN</v-btn
+          >
+        </v-col>
+        <v-col cols="6">
+          <img src="~@/assets/img/guy-girl-in-library.png" width="100%" />
+        </v-col>
+      </v-row>
+      <!-- <v-row>
+          <v-col cols="6">
+            <img src="~@/assets/guy-girl-in-library.png" width="100%" />
+          </v-col>
+          <v-col cols="6">
+            <h1>banana</h1>
+          </v-col>
+        </v-row> -->
+    </v-col>
+  </v-row>
 </template>
 <script>
   export default {
     name: 'IndexPage',
-    layout: 'administrator',
+    layout: 'guest',
   }
 </script>
