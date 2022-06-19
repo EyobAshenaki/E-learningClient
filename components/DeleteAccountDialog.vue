984726621925cd8c21b67e090a08ef0c42158a41
Ezira Ashenafi <eazash22@gmail.com>
2022-06-19 21:06:48 +0300

    feat: add account edit and delete

--- components/DeleteAccountDialog.vue
+++ components/DeleteAccountDialog.vue
@@ -5,27 +5,31 @@
         <v-icon>mdi-delete</v-icon>
       </v-btn>
     </template>
-    <v-card>
+    <v-card v-if="dialog">
       <v-card-title>Delete Account</v-card-title>
       <v-card-text>
         <p>Are you sure you want to delete this account?</p>
+        <p>
+          Account name: {{fullName}}
+        </p>
       </v-card-text>
       <v-card-actions>
-        <v-btn color="error" outlined @click="deleteAccount">Delete</v-btn>
+        <v-btn color="error" :disabled="loading" :loading="loading" outlined @click="deleteAccount">Delete</v-btn>
         <v-spacer />
-        <v-btn text @click.stop="dialog = false">Cancel</v-btn>
+        <v-btn text :loading="loading" @click.stop="dialog = false">Cancel</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+  import { REMOVE_USER } from '~/utils/queries'
   export default {
     props: {
       account: {
         type: Object,
         default: () => ({
-          dbId: '',
+          id: '',
           email: '',
           firstName: '',
           lastName: '',
@@ -36,15 +40,47 @@
     },
     data() {
       return {
+        loading: false,
         dialog: false,
       }
     },
+    computed: {
+      fullName() {
+        return [
+          this.account.firstName,
+          this.account.middleName,
+          this.account.lastName,
+        ].join(' ')
+      },
+    },
     methods: {
-      deleteAccount() {
+      async deleteAccount() {
+        this.loading = true
         try {
-          // call api delete route
+          await this.$axios
+            .post('/graphql', {
+              query: REMOVE_USER,
+              variables: {
+                userId: this.account.id,
+              },
+            })
+            .then(({ data }) => {
+              if (data.errors) {
+                throw data.errors
+              } else if (data.data) {
+                return data.data
+              }
+            })
+          this.$toast.success('Account deleted successfully')
+          this.$emit('update:account')
         } catch (error) {
+          if (error?.isAxiosError && error.response) {
+            this.$toast.error(error.response.errors)
+          } else {
+            this.$toast.error(error.message)
+          }
         } finally {
+          this.loading = false
           this.dialog = false
         }
       },
--- utils/validators.js
+++ utils/validators.js
@@ -1,2 +1,5 @@
 export const singleCharacter = (val) =>
   val.length === 1 || 'Only a single Character allowed'
+
+export const singleWord = (val) =>
+  val.split(' ').length === 1 || 'Only single word allowed'
