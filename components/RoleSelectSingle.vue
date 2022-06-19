e75b3afc42f825dd83178b1da05a2d30ff184163
Ezira Ashenafi <eazash22@gmail.com>
2022-06-20 00:47:28 +0300

    feat: add user account create and import

---
+++ components/RoleSelectSingle.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-select v-model="role" v-bind="$attrs" :items="roles" outlined label="Select Role" @change="update"></v-select>
+</template>
+
+<script>
+  export default {
+    model: {
+      prop: 'roleInput',
+      event: 'selected',
+    },
+    props: {
+      roleInput: {
+        type: String,
+        default: '',
+      },
+    },
+    data() {
+      return {
+        role: '',
+        roles: [
+          'ADMINISTRATOR',
+          'TEACHER',
+          'STUDENT',
+          'COURSE_MANAGER',
+          'DEPARTMENT_ADMINISTRATOR',
+        ],
+      }
+    },
+    methods: {
+      update() {
+        this.$emit('selected', this.role)
+      },
+    },
+  }
+</script>
