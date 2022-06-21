16be9b4fc83e8f15d2c43fd5bbcd7e04c71851c7
EyobAshenaki <ethioeyoba@gmail.com>
2022-06-22 01:00:48 +0300

    feat(studentPage): connect quiz page with back-end
    - patched quiz attempt page routing

--- pages/student/_id/quizzes/index.vue
+++ pages/student/_id/quizzes/index.vue
@@ -3,43 +3,47 @@
     <v-col cols="12" class="d-flex align-center">
       <h2 style="display: inline">All Tests</h2>
       <v-divider class="mx-2 my-0" vertical></v-divider>
-      <span>3 in total</span>
+      <span v-if="quizzes"> {{ quizzes.length }} in total</span>
     </v-col>
     <v-col cols="12">
       <v-card
-        v-for="n in 10"
-        :key="n"
+        v-for="quiz in quizzes"
+        :key="quiz.id"
         max-width="360"
         elevation="14"
         class="ma-3"
         style="display: inline-block"
       >
-        <v-card-title>Exam-1</v-card-title>
-        <v-card-subtitle>Database Design</v-card-subtitle>
+        <v-card-title>
+          {{ quiz.title }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{ quiz.description }}
+        </v-card-subtitle>
 
         <v-divider class="mx-4"></v-divider>
 
         <v-row class="pa-6 pb-0">
           <v-col cols="5" class="px-0">
-            <v-icon>mdi-calendar-range</v-icon>21 Oct, 1:00pm
+            <v-icon>mdi-calendar-range</v-icon> {{ formatDate(quiz.start) }}
           </v-col>
           <v-col cols="2" class="px-0 d-flex justify-center">
             <v-icon>mdi-arrow-right</v-icon>
           </v-col>
           <v-col cols="5" class="px-0">
-            <v-icon>mdi-calendar-range</v-icon>24 Oct, 7:00pm
-          </v-col>
-          <v-col cols="6"> <v-icon>mdi-timer-outline</v-icon> 3 min </v-col>
-          <v-col cols="6">
-            <v-icon>mdi-chart-sankey-variant</v-icon> 20 Questions
+            <v-icon>mdi-calendar-range</v-icon> {{ formatDate(quiz.end) }}
           </v-col>
           <v-col cols="6">
-            <v-icon>mdi-account-multiple-outline</v-icon> 200 students
+            <v-icon>mdi-timer-outline</v-icon>
+            {{ quiz.duration / 60000 }} min
+          </v-col>
+          <v-col cols="6">
+            <v-icon>mdi-chart-sankey-variant</v-icon> Grade {{ quiz.maxScore }}%
           </v-col>
         </v-row>
 
         <v-card-actions class="pl-6 py-5">
-          <v-btn depressed @click="gotoQuizPage">Start now</v-btn>
+          <v-btn depressed @click="gotoQuizPage(quiz.id)">Start now</v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -47,20 +51,69 @@
 </template>
 
 <script>
-export default {
-  layout: 'student',
+  export default {
+    layout: 'student',
 
-  data() {
-    return {}
-  },
-
-  methods: {
-    gotoQuizPage() {
-      this.$router.push({
-        name: 'student-id-quizzes-quizPage',
-        params: { id: 1 },
-      })
+    data() {
+      return {
+        quizzes: [],
+      }
     },
-  },
-}
+
+    created() {
+      this.initialize()
+    },
+
+    methods: {
+      async initialize() {
+        const query = `query quizzes {
+                      quizzes {
+                        id
+                        title
+                        description
+                        start
+                        end
+                        duration
+                        maxScore
+                        sections {
+                          id
+                          description
+                          sectionType
+                          number
+                          questions {
+                            id
+                            text
+                            number
+                            questionType
+                          }
+                        }
+                      }
+                    }`
+
+        const quizzesResponse = await this.$axios.post('/graphql', {
+          query,
+        })
+
+        this.quizzes = quizzesResponse.data.data.quizzes
+      },
+
+      formatDate(dateObj) {
+        const date = new Date(dateObj)
+
+        return `${date.getDate()} ${
+          date.toString().split(' ')[1]
+        }, ${date.getHours()}:${date.getMinutes().toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}`
+      },
+
+      gotoQuizPage(quizId) {
+        this.$router.push({
+          name: 'student-id-quizzes-quizPage',
+          params: { id: this.$nuxt.context.params.id, quizId },
+        })
+      },
+    },
+  }
 </script>
