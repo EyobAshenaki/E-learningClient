561a252af02ba83d2586ec6c7b0d6d544da29b74
Ezira Ashenafi <eazash22@gmail.com>
2022-06-22 00:30:52 +0300

    feat: cleanup course manager sidebar

--- components/AdminSideBar.vue
+++ components/AdminSideBar.vue
@@ -1,21 +1,5 @@
 <template>
-  <v-navigation-drawer v-model="drawer" clipped app permanent>
-    <v-list>
-      <v-tooltip v-for="item in items" :key="item.title" right>
-        <template #activator="{ on, attrs }">
-          <v-list-item v-bind="attrs" exact nuxt :to="item.route" v-on="on">
-            <v-list-item-icon>
-              <v-icon size="30">{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-        <span>{{ item.description || item.title }}</span>
-      </v-tooltip>
-    </v-list>
-  </v-navigation-drawer>
+  <SideBar :items="items" />
 </template>
 
 <script>
@@ -39,7 +23,11 @@
             icon: 'mdi-bookshelf',
             route: '/administrator/students',
           },
-          { title: 'Teachers', icon: 'mdi-note-edit', route: '/administrator/teachers' },
+          {
+            title: 'Teachers',
+            icon: 'mdi-note-edit',
+            route: '/administrator/teachers',
+          },
           {
             title: 'Course Managers',
             icon: 'mdi-text-box-check',
---
+++ components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-navigation-drawer v-model="drawer" clipped app permanent>
+    <v-list>
+      <v-tooltip v-for="item in items" :key="item.title" right>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" exact nuxt :to="item.route" v-on="on">
+            <v-list-item-icon>
+              <v-icon size="30">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+        <span>{{ item.description || item.title }}</span>
+      </v-tooltip>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    props: {
+      items: {
+        type: Array,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        drawer: false,
+      }
+    },
+  }
+</script>
