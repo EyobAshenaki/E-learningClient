05ffa52a9edc5f3a3964db1311aa7b325f1c1404
Ezira Ashenafi <eazash22@gmail.com>
2022-06-23 07:53:22 +0300

    style: refactore course card

--- components/CourseCardMini.vue
+++ components/CourseCardMini.vue
@@ -1,115 +1,54 @@
 <template>
   <!-- todo! complete course details on this page -->
   <!-- todo! complete tooltips for all icon-based buttons -->
-  <v-card elevation="6">
-    <v-row>
-      <!-- Course card head section -->
-      <v-col cols="12">
-        <v-row>
-          <v-col cols="3" class="d-flex justify-center align-center">
-            <v-avatar size="90" color="grey lighten-3">
-              <v-icon size="60" color="#25327F">mdi-book-open-outline</v-icon>
-            </v-avatar>
-          </v-col>
-          <v-col cols="9">
-            <v-row>
-              <v-col cols="12" class="pb-0">
-                <v-card-title style="word-break: break-word" class="px-0">{{
-                  course.name
-                }}</v-card-title>
-                <v-card-subtitle class="px-0 pb-0">{{
-                  course.code
-                }}</v-card-subtitle>
-              </v-col>
-              <!-- Course Button Group -->
-              <v-col cols="12" class="p-0">
-                <v-btn-toggle>
-                  <v-btn
-                    nuxt
-                    :to="`${$route.path}/${course.id}/course-documents`"
-                  >
-                    <!-- todo tooltip -->
-                    <!-- @click="assignCourseTeacher(course.id)"
-                    @mouseenter="assignEnter($event)"
-                    @mouseleave="leaveBtn()"
-                  > -->
-                    <v-icon color="orange darken-4">mdi-file-upload</v-icon>
-                  </v-btn>
-                  <v-btn nuxt :to="`${$route.path}/${course.id}/quizzes`">
-                    <!-- todo tooltip -->
-                    <!-- @click="assignCourseTeacher(course.id)"
-                    @mouseenter="assignEnter($event)"
-                    @mouseleave="leaveBtn()"
-                  > -->
-                    <v-icon color="#25327F">mdi-text-box-plus</v-icon>
-                  </v-btn>
-                  <v-btn>
-                    <!-- todo tooltip -->
-                    <!-- @click="assignCourseTeacher(course.id)"
-                    @mouseenter="assignEnter($event)"
-                    @mouseleave="leaveBtn()"
-                  > -->
-                    <v-icon color="amber darken-3"
-                      >mdi-clipboard-text-clock</v-icon
-                    >
-                  </v-btn>
-                </v-btn-toggle>
-
-                <!-- Button group Tooltips -->
-                <v-tooltip
-                  v-model="showTooltip"
-                  top
-                  open-delay="1000"
-                  close-delay="1000"
-                  :nudge-right="tooltipPositionX"
-                  :nudge-bottom="tooltipPositionY"
-                >
-                  {{ tooltipText }}
-                </v-tooltip>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-        <v-divider class="mt-5 mx-6"></v-divider>
+  <v-card outlined class="py-4 px-2">
+    <!-- Course card head section -->
+    <v-row justify="space-around" class="px-2">
+      <v-col cols="4" align-self="center">
+        <v-avatar size="90" color="grey lighten-3">
+          <v-icon size="60" color="#25327F">mdi-book-open-outline</v-icon>
+        </v-avatar>
       </v-col>
-
-      <!-- todo course details -->
-      <!-- Course card body section -->
-      <!-- <v-col cols="12">
-        <v-row class="mx-1 d-flex justify-space-around">
-          <!- <v-spacer></v-spacer> --
-
-          <v-col cols="5" class="pt-0">
-            <v-list-item class="px-0">
-              <v-list-item-avatar color="grey lighFten-3">
-                <v-icon color="#25327F"
-                  >mdi-book-open-page-variant-outline</v-icon
-                >
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ course.chapters.length }}
-                </v-list-item-title>
-                <v-list-item-subtitle> Chapters </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-          <v-col cols="5" class="pt-0 px-0">
-            <v-list-item class="px-0">
-              <v-list-item-avatar color="grey lighten-3">
-                <v-icon color="#25327F">mdi-timer-outline</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ course.creditHour }}
-                </v-list-item-title>
-                <v-list-item-subtitle> Credit Hour </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-        </v-row>
-      </v-col> -->
+      <v-col cols="8">
+        <v-card-title style="word-break: break-word">{{
+          course.name
+        }}</v-card-title>
+        <v-card-subtitle>{{ course.code }}</v-card-subtitle>
+      </v-col>
     </v-row>
+    <v-card-text>
+      <!-- Course Button Group -->
+      <v-row>
+        <v-col>
+          <v-btn
+            nuxt
+            :to="`${$route.path}/${course.id}/course-documents`"
+            block
+          >
+            Course Documents
+            <v-icon color="orange darken-4" class="ml-2">mdi-file</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn nuxt :to="`${$route.path}/${course.id}/quizzes`" block>
+            Quizzes
+            <v-icon color="#25327F" class="ml-2">mdi-text-box</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn block>
+            Assignments
+            <v-icon color="amber darken-3" class="ml-2"
+              >mdi-clipboard-text-clock</v-icon
+            >
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -119,22 +58,6 @@
       course: {
         type: Object,
         required: true,
-      },
-    },
-    data() {
-      return {
-        tooltipValue: '',
-        showTooltip: false,
-        tooltipPositionX: null,
-        tooltipPositionY: null,
-      }
-    },
-    computed: {
-      tooltipText() {
-        if (this.tooltipValue === 'assign') return 'Assign Teacher'
-        else if (this.tooltipValue === 'edit') return 'Edit Course'
-        else if (this.tooltipValue === 'delete') return 'Delete Course'
-        else return null
       },
     },
   }
